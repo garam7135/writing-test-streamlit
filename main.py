eb99936a62991313690b267aaab227072ab35499
garam7135 <151899519+garam7135@users.noreply.github.com>
2024-06-25 18:31:33 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 '''
 
-독후감 및 책 요약문 생성 PoC main
+독후감 및 책 요약문 생성 Test main
 
 '''
 import streamlit as st
@@ -24,38 +24,38 @@ st.markdown(
         font-size: 20px;
         text-align: center;
     }
+    .ssmall-font {
+        font-size: 13px;
+        text-align: left;
+        padding: 0px;
+    }
     </style>
     """,
     unsafe_allow_html=True
 )
-# def get_book_file_list(folder_path=""):
-#     filenames = os.listdir(folder_path)
-#     selected_filename = st.selectbox('선택 가능한 도서 리스트', filenames)
-#     return selected_filename
 
+# main 실행부
 def main():
-    # streamlit으로 제목과 input box 생성
+    # streamlit으로 제목 생성
     st.title("독후감 & 요약 PoC")
     with st.container(border=True):
-        # selected_filename = get_book_file_list(folder_path=BOOK_TEXT_PATH)
-        # st.write('선택한 책 이름: `%s`' % selected_filename)
-        # if st.checkbox('책 원문 내용 확인하기') :
-        #     book_text = codecs.open(os.path.join('./book_text', selected_filename)).read()
-        #     st.write(book_text)
-        # else :
-        #     st.text('원문 숨기기 상태.')
-        content = st.text_area("책의 원문 내용을 입력해 주세요. (최대 35000자 입력 가능)")
-        interesting = st.text_input("책에서 가장 재밌었던 부분을 입력해 주세요. (최대 50자 입력 가능)")
-        impression = st.text_input("책을 통해 느낀점을 입력해 주세요. (최대 50자 입력 가능)")
+        st.text("주의 사항")
+        st.markdown('<p class="ssmall-font">1. 입력되지 않은 값에 대해서는 랜덤으로 생성됩니다. 필요한 부분은 반드시 입력해 주세요.</p>', unsafe_allow_html=True)
+        st.markdown('<p class="ssmall-font">2. 책에 존재하지 않는 내용을 입력할 시 새로운 창작 결과가 생성될 수 있습니다.</p>', unsafe_allow_html=True)
 
-    # 컨테이너 안에 두 가지 output 결과를 좌우로 배치
+    with st.container(border=True):
+        content = st.text_area("책의 원문을 입력해 주세요. (최대 40,000자 입력 가능)", max_chars=40000)
+        interesting = st.text_input("책에서 가장 재밌었던 부분을 입력해 주세요. (최대 50자 입력 가능)", max_chars=50)
+        impression = st.text_input("책을 통해 느낀점을 입력해 주세요. (최대 50자 입력 가능)", max_chars=50)
+
+    # 결과 보기 버튼 누름시, 컨테이너 안에 두 가지 output 결과를 좌우로 배치
     if st.button("결과 보기"):
         with st.container():            
             # 좌우로 나눌 컬럼 설정
             col1, col2 = st.columns(2)
             
             # 첫 번째 컬럼: 독후감 결과
-            with st.spinner("독후감 및 요약문이 작성 중입니다... 원문 내용에 따라 최대 1~2분 정도 소요됩니다."):
+            with st.spinner("독후감 및 요약문이 작성 중입니다... 책 원문 길이에 따라 최대 1~2분 정도 소요됩니다."):
                 sum_result, bookreport_result = langchain_model(text=content, interesting=interesting, impression=impression)
                 with col1:
                     st.markdown('<div class="column">', unsafe_allow_html=True)
@@ -70,7 +70,5 @@ def main():
                     st.write(sum_result)
                     st.markdown('</div>', unsafe_allow_html=True)
             
-            st.markdown('</div>', unsafe_allow_html=True)
-
 if __name__ == '__main__':
     main()
--- model.py
+++ model.py
@@ -2,7 +2,6 @@ from langchain.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
 from langchain.chains import LLMChain, ReduceDocumentsChain, MapReduceDocumentsChain
 from langchain.chains.combine_documents.stuff import StuffDocumentsChain
-from langchain.document_loaders import TextLoader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.docstore.document import Document
 
@@ -14,7 +13,7 @@ def tokenizer(text):
 
     # 스플리터 지정
     # 가장 긴 눈의 여왕 텍스트 수 : 34,000자. 
-    # token 수: 약 34000자 
+    # token 수: 약 35,000
     # OpenAI 모델 gpt-3.5-turbo-0125 토큰 수 : 16,385 tokens -> spliter 필요
     text_splitter = CharacterTextSplitter.from_tiktoken_encoder(
         separator="\n",  # 분할기준
@@ -32,20 +31,18 @@ def tokenizer(text):
     
     return split_docs
 
-def langchain_model(text="책원문", interesting="재미있던 부분", impression="느낀점"):
+def langchain_model(text="책 줄거리 랜덤 생성", interesting="재미있던 부분 랜덤 생성", impression="느낀점 랜덤 생성"):
 
     '''
     - 요약/ 독후감 langchain model
     '''
 
-    ''' 입력 범위 제한'''
-    text = text[:40000]
-    interesting = interesting[:50]
-    impression = impression[:50]
-
     split_docs = tokenizer(text)
+    llm = ChatOpenAI(streamlit.secrets["OPENAI_API_KEY"],
+                    temperature=0,
+                    model_name='gpt-3.5-turbo-0125')
 
-    ################## 1. Map 단계: split한 문서를 각각 요약하는 단계 ##################
+    ''' 1. Map 단계 '''
     # Map 프롬프트
     map_template = """다음 입력은 책 줄거리의 일부분입니다.
     {pages}
@@ -53,10 +50,10 @@ def langchain_model(text="책원문", interesting="재미있던 부분", impress
     """
     map_template+=f"""{interesting}
     책 줄거리 일부분에 대해 주요 내용을 요약해 주세요.
-    - 요약에 들어가야 할 내용과 줄거리가 일치하는 부분이 있다면, 해당 내용을 반드시 포함시켜 주세요.
-    - 책 줄거리에 주어진 내용만을 사용해 요약을 작성하세요. 새로운 내용을 추가하지 마세요.
-    - 문맥을 자연스럽게 유지해 주세요.
-    - 책 제목을 첫 번째 줄에 포함시켜주세요.
+    - 요약에 들어가야 할 내용이 줄거리에 있다면, 해당 내용을 반드시 포함시켜 주세요.
+    - 책 줄거리에 주어진 내용만을 사용해 요약을 작성하세요. 책 줄거리에 없는 내용은 생성하지 말아야 합니다.
+    - 문맥이 자연스러운지 한번 더 확인해 주세요.
+    - 첫 줄에 책 제목을 포함해 주세요.
     """
 
     # Map 프롬프트 정의
@@ -64,12 +61,9 @@ def langchain_model(text="책원문", interesting="재미있던 부분", impress
     print(map_prompt)
 
     # Map LLMChain 정의
-    llm = ChatOpenAI(openai_api_key =streamlit.secrets["OPENAI_API_KEY"],
-                    temperature=0,
-                    model_name='gpt-3.5-turbo-0125')
     map_chain = LLMChain(llm=llm, prompt=map_prompt)
 
-    ################## 2. Reduce 단계: 각각의 문서를 하나로 요약하는 단계 ##################
+    ''' 2. Reduce 단계: 각각의 문서를 하나로 요약하는 단계 '''
     # Reduce 프롬프트
     reduce_template = """다음은 책 줄거리 요약의 리스트입니다:
     {doc_summaries}
@@ -78,11 +72,12 @@ def langchain_model(text="책원문", interesting="재미있던 부분", impress
     reduce_template += f"""
     {interesting}
     위 내용을 바탕으로 자연스러운 통합된 요약을 만들어 주세요.
-    - 요약에 들어가야 할 내용을 포함시켜 주세요.
-    - 모든 내용을 자연스럽게 엮어 주세요.
-    - 요약에 들어가야 할 내용이 너무 많거나, 자연스럽지 못한 부분이 있는지 한번 더 검토해 주세요.
-    - 통합된 요약은 반드시 책 줄거리 요약의 리스트를 참조해야 합니다. 없는 내용을 만들어 내지 마세요.
-    - 책 제목을 첫 번째 줄에 포함시켜주세요.
+    - 책의 중요한 줄거리들을 반영하여 내용을 자세하게 요약해 주세요.
+    - 요약에 들어가야 할 내용이 줄거리에 있다면, 해당 내용을 반드시 포함시켜 주세요.
+    - 책 줄거리에 주어진 내용만을 사용해 요약을 작성하세요. 책 줄거리에 없는 내용은 생성하지 말아야 합니다.
+    - 문맥이 자연스러운지 한번 더 확인해 주세요.
+    - 첫 줄에 책 제목을 포함해 주세요.
+    - 최소 200자 ~ 최대 300자 이내로 작성해 주세요.
     """
 
     # Reduce 프롬프트 정의
@@ -92,7 +87,7 @@ def langchain_model(text="책원문", interesting="재미있던 부분", impress
     # Reduce에서 수행할 LLMChain 정의
     reduce_chain = LLMChain(llm=llm, prompt=reduce_prompt)
 
-    ################## 3. Map-Reduce 단계 ##################
+    ''' 3. Map-Reduce 단계'''
     # 문서 리스트를 단일 문자열로 결합하고, 이를 LLMChain에 전달
     combine_documents_chain = StuffDocumentsChain(
         llm_chain=reduce_chain,                
@@ -121,34 +116,34 @@ def langchain_model(text="책원문", interesting="재미있던 부분", impress
 
     # 독후감 프롬프트
     report_template = """ 아래 내용을 바탕으로 독후감을 작성해 주세요.
-    [책 줄거리], [재미있던 내용], [느낀점]을 반드시 아래 형식에 맞게 반영해 주세요.
+    입력된 책 줄거리, 재미있던 내용, 느낀점을 반드시 독후감 형식에 적절하게 반영해 주세요.
 
     독후감 형식:
+    1.
+    - 요소: 독후감 제목, 책 제목, 책을 읽은 동기
+    - 설명: 독후감 제목을 정하고, 책 제목을 부제목으로 붙인 뒤, 이책을 왜 읽게 되었는지를 작성해 주세요.
+    2.
+    - 요소: 책 줄거리, 기억에 남거나 감동 받은 장면, 재미있게 느낀 내용
+    - 설명: 책 줄거리를 간단하게 요약하고, 기억에 남는 장면, 재미있던 내용 등을 담아 작성해 주세요.
+    3.
+    - 요소: 전체적인 느낌점 및 감상, 반성한 점, 새로운 다짐이나 결심
+    - 설명: 책을 읽고 난 후 들었던 생각과, 느낀 점을 정리하는 부분으로 자신만의 느낌이나 교훈 등을 담아 작성해 주세요.
 
-    처음:
-    - 독후감 제목, 책 제목, 책을 읽은 동기
-    - 독후감 제목을 정하고, 책 제목을 부제목으로 붙인 뒤, 왜 이책을 읽게 되었는지를 작성해 주세요.
-    중간:
-    - 책 줄거리, 기억에 남거나 감동 받은 장면, 재미있게 느낀 내용
-    - 책 줄거리를 간단하게 요약하고, 기억에 남는 장면, 재미있던 내용 등을 담아 작성해 주세요.
-    끝:
-    - 전체적인 느낌점 및 감상, 반성한 점, 새로운 다짐이나 결심
-    - 책을 읽고 난 후 들었던 생각과, 느낀 점을 정리하는 부분으로 자신만의 느낌이나 교훈 등을 담아 작성해 주세요.
+    위 내용들을 순서대로 작성하여 하나의 독후감을 만들어 주세요.
+    번호, 요소, 형식 등은 출력하지 마세요.
 
-    독후감은 형식을 유지하지만 처음, 중간, 끝이라는 키워드를 출력하지 마세요.
-
-    [책 줄거리]
+    책 줄거리:
     {text}
 
     """
     report_template += f"""
-    [재미있던 내용]:
+    재미있던 내용:
     {interesting}
 
-    [느낀점]:
+    느낀점:
     {impression}
 
-    [생성된 독후감]:
+    생성된 독후감:
     """
     
     # 독후감 프롬프트 정의
@@ -165,5 +160,5 @@ def langchain_model(text="책원문", interesting="재미있던 부분", impress
     return sum_result['output_text'], bookreport_result['text']
 
 if __name__ == '__main__':
-    # test code
-    sum_result, bookreport_result = langchain_model(text="인어공주는", interesting="인어 공주가 승천하는 부분", impression="착하게 살아야 한다.")
+    # 생성결과 test
+    sum_result, bookreport_result = langchain_model(text="책 줄거리 랜덤 생성", interesting="재미있던 부분 랜덤 생성", impression="느낀점 랜덤 생성")
